99893511d767621b58172d1159f6b00f97fb1c71
Patriot <patriotina@gmail.com>
2018-10-15 17:45:53 +0400

    Initial commit

---
+++ src/Cell.java
@@ -0,0 +1,41 @@
+import java.awt.Color;
+import java.awt.Graphics;
+
+/**
+ * Java. Classic Game Snake
+ *  Class Cell: minimal building element
+ *
+ * @author Sergey Iryupin
+ * @version 0.3 dated Aug 23, 2018
+ */
+
+public class Cell {
+    protected int x, y;                       // object coordinates
+    protected int size;                       // object size in px
+    protected Color color;                    // object color
+
+    public Cell(int x, int y, int size, Color color) {
+        set(x, y);                            // init coordinates
+        this.size = size;                     // init size
+        this.color = color;                   // init color
+    }
+
+    public void set(int x, int y) {           // set coordinates
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {                       // get the X coordinate
+        return x;
+    }
+
+    public int getY() {                       // get the Y coordinate
+        return y;
+    }
+
+    public void paint(Graphics g) {           // object rendering
+        g.setColor(color);
+        g.fillOval(x * size, y * size,        // upper left corner
+                size, size);                  // width and height
+    }
+}
